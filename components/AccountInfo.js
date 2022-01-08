341e38ffac2e6487d7c64626c5a51bdea8f435c5
zachhuang610 <zachary_huang@brown.edu>
2022-01-07 20:38:04 -0500

    feat: role radio selection

--- components/AccountInfo.js
+++ components/AccountInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex, Text, Label, Button, Input } from 'theme-ui';
+import { Box, Flex, Radio, Divider, Label, Button, Input } from 'theme-ui';
 
 const AccountSettings = () => {
   return (
@@ -15,9 +15,24 @@ const AccountSettings = () => {
         <Label htmlFor="username">Username</Label>
         <Input id="username" defaultValue="@peduarte" />
       </Box>
+      <Box>
+        <Label>Role</Label>
+        <Flex mb={3}>
+          <Label>
+            <Radio name="letter" /> Learner
+          </Label>
+          <Label>
+            <Radio name="letter" /> Educator
+          </Label>
+          <Label>
+            <Radio name="letter" /> Editor
+          </Label>
+        </Flex>
+      </Box>
       <Flex css={{ marginTop: 20, justifyContent: 'flex-end' }}>
         <Button>Save changes</Button>
       </Flex>
+      <Divider />
       <Box>
         <Label htmlFor="currentPassword">Current password</Label>
         <Input id="currentPassword" type="password" />
